56653aec9f83c81c4dd7391035cb31e5241c736f
github username <github email address>
2019-12-06 15:25:53 -0600

    more node work

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const placesRoutes = require("./routes/places-routes");
+
 const app = express();
 
-
+//'places' routes...
+app.use('/api/places', placesRoutes);
 
 app.listen(5000);
