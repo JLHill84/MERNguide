fca451c35ca0ef6d7672c8f0ee4721d262efbcbd
github username <me@joshuahill.dev>
2019-12-12 18:04:56 -0600

    woof

---
+++ backend/util/keys.js
@@ -0,0 +1,9 @@
+const keys = {
+  DB_USER: "josh",
+  DB_PASS: "Ch33syrice",
+  DB_NAME: "ohPlaces",
+  GOOGLE_KEY: "AIzaSyBEm32aBiHXg3Lq8lm707guvOLvDXI1_ZA",
+  JOT_KEY: "top_secret_server_key"
+};
+
+module.exports = keys;
