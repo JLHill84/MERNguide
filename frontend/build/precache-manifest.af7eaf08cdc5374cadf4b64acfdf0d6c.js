e99f94596154e3f830afaa17af2ab6ec2286cc74
github username <me@joshuahill.dev>
2019-12-12 20:59:47 -0600

    build ver

---
+++ frontend/build/precache-manifest.af7eaf08cdc5374cadf4b64acfdf0d6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f7d84bf8bf04ba779713eeab60f528",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6afc9a8cf952e17896ca",
+    "url": "/static/css/main.83085e05.chunk.css"
+  },
+  {
+    "revision": "49ad6c7584f063692930",
+    "url": "/static/js/2.f07e0114.chunk.js"
+  },
+  {
+    "revision": "6afc9a8cf952e17896ca",
+    "url": "/static/js/main.7d5a7078.chunk.js"
+  },
+  {
+    "revision": "4c40b94f0748754179e8",
+    "url": "/static/js/runtime-main.bbc06ef5.js"
+  }
+]);
