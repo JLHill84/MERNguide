3deae52ca89e07b3dbf49eda345c6002550fd4d7
github username <me@joshuahill.dev>
2020-08-23 16:44:31 -0500

    commit before remove locally

--- backend/app.js
+++ backend/app.js
@@ -48,7 +48,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-e4lvt.gcp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://josh:Ch33syrice@cluster0-e4lvt.gcp.mongodb.net/mern?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5000);
